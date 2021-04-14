d6db9dc883d5cdfbfd421218cfa99b56c241af80
shutupRohit <shutupRohit@gmail.com>
2021-04-14 14:05:07 +0530

    Delete Campground working

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const { campgroundSchema } = require('../joi/schemas');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const campGround = new Schema({
     title: String,
@@ -8,6 +10,18 @@ const campGround = new Schema({
     description: String,
     location: String,
     reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}]
+})
+
+campGround.post('findOneAndDelete', async function(campground){
+    // console.log(campground);
+    if(campground){
+        await Review.deleteMany({
+            _id: {
+                $in: campground.reviews
+            }
+        })
+    }
+    
 })
 
 module.exports = mongoose.model('campGround', campGround);
